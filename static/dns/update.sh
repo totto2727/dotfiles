de0c29a12dc0eac568b6077024511f62b10d4810
totto2727 <kaihatu.totto2727@gmail.com>
2022-12-23 12:00:04 +0900

    NFSに証明書を転記するように設定

--- static/dns/update.sh
+++ static/dns/update.sh
@@ -3,4 +3,4 @@ export CF_TOKEN=$(cat ~/.dehydrated/cloudflare.token)
 cd ~/dehydrated
 ./dehydrated --config ~/.dehydrated/config -c > /tmp/dehydrated.log 2> /tmp/dehydrated_err.log
 
-#docker exec nginx service nginx reload
+rsync -a -v ~/.dehydrated/certs /var/nfs/general && chmod -R "=r,u+w,g+w,+X" /var/nfs/general/certs
