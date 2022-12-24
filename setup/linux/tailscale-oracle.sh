ea1b64a733bdbe5053fdc599cc9c4d4538252aab
totto2727 <kaihatu.totto2727@gmail.com>
2022-12-24 14:34:47 +0900

    NFSのためにサブネットを受け入れるように設定

--- setup/linux/tailscale-oracle.sh
+++ setup/linux/tailscale-oracle.sh
@@ -1,1 +1,1 @@
-sudo tailscale up --advertise-routes=10.0.0.0/24,169.254.169.254/32 --accept-dns=false
+sudo tailscale up --advertise-routes=10.0.0.0/24,169.254.169.254/32 --accept-dns=false --accept-routes
