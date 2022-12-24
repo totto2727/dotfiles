f36dc579150b356e15bff1855a31ffd0d20849e7
totto2727 <kaihatu.totto2727@gmail.com>
2022-12-24 11:45:47 +0900

    macのNFSのマウントを調整

--- setup/mount-nfs.sh
+++ setup/mount-nfs.sh
@@ -1,1 +1,2 @@
-sudo mount_nfs -o vers=4 -o bg -P dev-surface3.totto.page:/var/nfs/general $HOME/nfs/surface3
+mkdir -p $HOME/nfs/general
+sudo mount_nfs -o vers=4 -o bg -P dev-surface3.totto.page:/var/nfs/general $HOME/nfs/general
