bbeda7518cc8f4ebd37ea7b41ab1997fb03db0e3
totto2727 <kaihatu.totto2727@gmail.com>
2023-02-26 21:32:26 +0900

    クローン先が間違えていたので修正

--- install/mac/install.bash
+++ install/mac/install.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-test -d ~/git_config || git clone git@github.com:totto2727/git_config.git ~/ansible_playbooks
-test -d ~/ssh_config || git clone git@github.com:totto2727/ssh_config.git ~/ansible_playbooks
+test -d ~/git_config || git clone git@github.com:totto2727/git_config.git ~/git_config
+test -d ~/ssh_config || git clone git@github.com:totto2727/ssh_config.git ~/ssh_config
 test -d ansible_playbooks || git clone git@github.com:totto2727/ansible_playbooks.git ~/ansible_playbooks
 
 bash ~/dotfiles/install/mac/brew.bash
